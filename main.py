bcde644a93502b47f9412dc9429d0fafd75d0e8c
duongng2911 <21070546@vnu.edu.vn>
2024-11-10 20:05:02 +0700

    update api

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 import cv2
 # import secrets
 
@@ -11,7 +12,9 @@ from fastapi import FastAPI, UploadFile, File, Depends, status, HTTPException
 # from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from services.grounding_dino import ObjectDetectionServices
 from model.object_detection_view_model import ObjectDetectionViewModel
-
+from prometheus_client import Histogram
+from prometheus_fastapi_instrumentator import Instrumentator, metrics
+from prometheus_fastapi_instrumentator.metrics import Info
 
 class ResponseModel(BaseModel):
     response_data: list
@@ -29,6 +32,57 @@ model = {}
 # span_processor = BatchSpanProcessor(jaeger_exporter) # Manage Jaeger
 # trace_provider.add_span_processor(span_processor)
 
+NAMESPACE = os.environ.get("METRICS_NAMESPACE", "fastapi")
+SUBSYSTEM = os.environ.get("METRICS_SUBSYSTEM", "model")
+
+instrumentator = Instrumentator(
+    should_group_status_codes=True,
+    should_ignore_untemplated=True,
+    should_respect_env_var=True,
+    should_instrument_requests_inprogress=True,
+    excluded_handlers=["/metrics"],
+    inprogress_name="fastapi_inprogress",
+    inprogress_labels=True
+)
+
+# ----- add metrics -----
+instrumentator.add(
+    metrics.request_size(
+        should_include_handler=True,
+        should_include_method=True,
+        should_include_status=True,
+        metric_namespace=NAMESPACE,
+        metric_subsystem=SUBSYSTEM,
+    )
+)
+instrumentator.add(
+    metrics.response_size(
+        should_include_handler=True,
+        should_include_method=True,
+        should_include_status=True,
+        metric_namespace=NAMESPACE,
+        metric_subsystem=SUBSYSTEM,
+    )
+)
+instrumentator.add(
+    metrics.latency(
+        should_include_handler=True,
+        should_include_method=True,
+        should_include_status=True,
+        metric_namespace=NAMESPACE,
+        metric_subsystem=SUBSYSTEM,
+    )
+)
+instrumentator.add(
+    metrics.requests(
+        should_include_handler=True,
+        should_include_method=True,
+        should_include_status=True,
+        metric_namespace=NAMESPACE,
+        metric_subsystem=SUBSYSTEM,
+    )
+)
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     model["detector"] = ObjectDetectionServices("cpu") 
@@ -37,6 +91,7 @@ async def lifespan(app: FastAPI):
 
 # FastAPI
 app = FastAPI(lifespan=lifespan)
+instrumentator.instrument(app).expose(app, include_in_schema=False, should_gzip=True)
 
 @app.post("/detect", response_model=list[ObjectDetectionViewModel])
 async def detectObject(prompt: str, data: UploadFile = File(...)): 
